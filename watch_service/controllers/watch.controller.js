dc2cea8b8c6560c34018ed7a47bdaae222e2d1d7
Or Yehezkel <myavraham25@gmail.com>
2024-06-25 15:09:41 +0300

    Initialized Prisma, and refractored code

--- watch_service/controllers/watch.controller.js
+++ watch_service/controllers/watch.controller.js
@@ -1,37 +1,12 @@
-import AWS from 'aws-sdk'
-
-async function generateSignedUrl(videoKey) {
-  const s3 = new AWS.S3({
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    region: 'ap-south-1',
-  })
-
-  const params = {
-    Bucket: process.env.AWS_BUCKET,
-    Key: videoKey,
-    Expires: 3600, // URL expires in 1 hour, adjust as needed
-  }
-
-  return new Promise((resolve, reject) => {
-    s3.getSignedUrl('getObject', params, (err, url) => {
-      if (err) {
-        reject(err)
-      } else {
-        resolve(url)
-      }
-    })
-  })
-}
-
+const baseUrl = 'https://dg36nmbukx5oh.cloudfront.net/'
 const watchVideo = async (req, res) => {
   try {
     const videoKey = req.query.key // Key of the video file in S3
     console.log('video key is ', videoKey)
     // use CloudFront instead
-    const signedUrl = await generateSignedUrl(videoKey)
-    res.json({ signedUrl })
-    console.log(signedUrl)
+    const url = baseUrl + videoKey
+    res.json({url})
+    console.log(url)
   } catch (err) {
     console.error('Error generating pre-signed URL:', err)
     res.status(500).json({ error: 'Internal Server Error' })
